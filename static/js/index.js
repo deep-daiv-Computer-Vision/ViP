9bb33d547543b95671fb5b3feb49c197c1cb2bb1
jaeung <dlwodnd00@gmail.com>
2024-07-01 20:28:32 +0900

    add templete

--- static/js/index.js
+++ static/js/index.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
 
     var options = {
     slidesToScroll: 1,
-    slidesToShow: 2.3,
+    slidesToShow: 1.25,
     centerMode: true, // Enable center mode
     loop: true,
     infinite: true,
